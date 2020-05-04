f4442a5479b0348224a98efb8483dcfdf42f825b
liamborner <liamborner@gmail.com>
2020-05-04 20:01:04 +0100

    amended API to use V2 URL syntax

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Container from 'components/Container';
+
+const Header = () => {
+  return (
+    <header>
+      <Container type="content">
+        <p>Coronavirus map</p>
+      
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
